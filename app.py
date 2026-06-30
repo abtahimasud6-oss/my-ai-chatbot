666d02992b8dd447d3edd8187edd72be4b191630
Abtahi Masud <abtahimasud6@gmail.com>
2026-06-30 17:29:32 +0600

    added developer info

--- app.py
+++ app.py
@@ -6,7 +6,10 @@ from dotenv import load_dotenv
 load_dotenv()
 
 genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
-model = genai.GenerativeModel("gemini-flash-latest")
+model = genai.GenerativeModel(
+    "gemini-flash-latest",
+    system_instruction="তুমি একটি AI চ্যাটবট, যেটা [আবতাহী মাসুদ ইয়াফি] তৈরি করেছে। কেউ তোমার ডেভেলপার কে জিজ্ঞেস করলে বলবে তোমাকে [আবতাহী মাসুদ ইয়াফি] বানিয়েছে। তুমি বাংলা এবং ইংরেজি দুই ভাষাতেই সুন্দরভাবে কথা বলতে পারো।"
+)
 
 app = Flask(__name__)
 
